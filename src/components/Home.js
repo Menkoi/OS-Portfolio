15b6a1933b0cd510924de147421e46c74f990684
celaeon <celaeon@gmail.com>
2021-07-20 18:26:53 -0400

    added folder icons, footer, and correct colors

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Home () {
+    return(
+        <section>
+          <div class="container">
+           <img class="img-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
+          </div>
+          <div class="container">
+           <img class="img-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
+          </div>
+          <div class="container">
+           <img class="img-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
+          </div>
+          <div class="container">
+           <img class="img-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
+          </div>
+        </section>
+        
+    )
+}
+
+export default Home;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer () {
+    return(
+        <section>
+            <div class="footer">
+               <footer class="footer">Logo Here</footer>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
