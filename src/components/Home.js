9376fc269747220e7816595761f3983b2b2f9aba
celaeon <celaeon@gmail.com>
2021-07-21 17:44:33 -0400

    fixed folders by changing png with text

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,33 +2,50 @@ import React from "react";
 
 function Home () {
     return(
-        <section>
-          <div class="flex-container">
-           <img class="folder-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
-           <p>About Me</p>
-          </div>
-          <div class="flex-container">
-           <img class="folder-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
-           <p>Portfolio</p>
-          </div>
-          <div class="flex-container">
-           <img class="folder-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
-           <p>Resume</p>
-          </div>
-          <div class="flex-container">
-           <img class="folder-thumbnail" src="https://i.imgur.com/liVt2oP.png" alt="" ></img>
-           <p>Contact Me</p>
+        <section >
+          <div class="folders">
+            <div class="content1">
+              <a href="test1">
+              <img src="https://i.imgur.com/UbyyT5e.png" class="folder-img" alt=""></img> 
+              </a>
+            </div>
+
+            <div class="content1">
+              <a href="test2">
+              <img src="https://i.imgur.com/LyYC0zw.png" class="folder-img" alt=""></img>
+              </a>
+            </div>
+
+            <div class="content1">
+              <a href="test3">
+              <img src="https://i.imgur.com/OStDTqu.png" class="folder-img" alt=""></img>
+              </a>
+            </div>
+
+            <div class="content1">
+              <a href="test3">
+              <img src="https://i.imgur.com/CZrtfWX.png" class="folder-img" alt=""></img>
+              </a>
+            </div>
           </div>
 
+          
+
         <div>
-          <div class="container-widgets">
+          <div class="widgets-1">
+            <a href="https://github.com/Menkoi">
             <img src="https://i.imgur.com/vd9FfAM.png" alt="" class="widget-thumbnail"></img>
+            </a>
           </div>
-          <div class="container-widgets">
+
+          <div class="widgets-1">
+            <a href="https://www.linkedin.com/in/brandon-burton-9b59a2215/">
             <img src="https://i.imgur.com/NFZhEsp.png" alt="" class="widget-thumbnail"></img>
+            </a>
           </div>
+
           <div class="container-widgets">
-            <img src="https://i.imgur.com/R0hub34.png" alt="" class="widget-thumbnail"></img>
+            <img src="" alt="" class="widget-thumbnail"></img>
           </div>
           </div>
         </section>
